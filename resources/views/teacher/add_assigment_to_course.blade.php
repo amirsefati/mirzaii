1964edb5e9bf5cfdb6d2d8f83b48e8e30f587138
amir <sefati100@gmail.com>
2020-10-11 22:35:28 +0330

    validation

--- resources/views/teacher/add_assigment_to_course.blade.php
+++ resources/views/teacher/add_assigment_to_course.blade.php
@@ -5,6 +5,17 @@
 @csrf
 <div class="row">
     <div class="col-md-12">
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        
         <div class="card mb-5">
             <div class="card-header">افزودن جلسه آموزشی به درس ({{$course->title}}  {{$course->grade}})</div>
                 <div class="card-body">

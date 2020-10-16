22da28644f3ba616e35782d107ccedc4b3ad8dcf
amir <sefati100@gmail.com>
2020-10-16 19:38:06 +0330

    exersice teacher

--- resources/views/teacher/add_assigment_to_course.blade.php
+++ resources/views/teacher/add_assigment_to_course.blade.php
@@ -19,111 +19,132 @@
         <div class="card mb-5">
             <div class="card-header">افزودن جلسه آموزشی به درس ({{$course->title}}  {{$course->grade}})</div>
                 <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4 mt-3">
-                        <label for="title">عنوان جلسه :</label>
-                        <input type="text" class="form-control" name="title">
-                    </div>
-                    <div class="col-md-8 mt-3">
-                        <label for="desc"> شرح مختصر جلسه :</label>
-                        <input type="text" class="form-control" name="desc">
+
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px">
+                    <p style="font-size:11px;color:#A63D40">پر کردن عنوان جلسه و شرح مختصر جلسه الزامی است</p>
+                    <div class="row">
+                        <div class="col-md-4 mt-3">
+                            <label for="title">عنوان جلسه :</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="col-md-8 mt-3">
+                            <label for="desc"> شرح مختصر جلسه :</label>
+                            <input type="text" class="form-control" name="desc">
+                        </div>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-md-12 mt-3">
-                        <label for="desc_2">توضیحات کامل :</label>
-                        <textarea name="desc_2" class="form-control" rows="4"></textarea>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4 mt-4">
-                        <label for="file_video">آپلود فایل صوتی :</label>
-                        <input type="file" name="file_video" class="form-control">
-                    </div>
-
-                    <div class="col-md-8 mt-4">
-                        <label for="file_video_title">توضیح فایل صوتی :</label>
-                        <input type="text" class="form-control" name="file_video_title">
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4 mt-2">
-                        <label for="file_video_2">آپلود ویدیو  :</label>
-                        <input type="file" name="file_video_2" class="form-control">
-                    </div>
-
-                    <div class="col-md-8 mt-2">
-                        <label for="file_video_2_title">توضیح فایل ویدیویی  :</label>
-                        <input type="text" class="form-control" name="file_video_2_title">
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40">پر کردن این بخش الزامی نیست، توضیح کامل جلسه را میتوانید وارد کنید</p>
+                    <div class="row">
+                        <div class="col-md-12 mt-3">
+                            <label for="desc_2">توضیحات کامل :</label>
+                            <textarea name="desc_2" class="form-control" rows="5"></textarea>
+                        </div>
                     </div>
                 </div>
 
 
-                <div class="row">
-                    <div class="col-md-4 mt-2">
-                        <label for="file_doc">آپلود فایل متنی :</label>
-                        <input type="file" name="file_doc_2" class="form-control">
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40">محتوای کلاس، در این بخش میتوانید فایل های جلسه خورد را آپلود کنید</p>
+                 
+                    <div class="row">
+                        <div class="col-md-4 mt-4">
+                            <label for="file_video">آپلود فایل صوتی :</label>
+                            <input type="file" name="file_video" accept="audio/*" class="form-control">
+                        </div>
+
+                        <div class="col-md-8 mt-4">
+                            <label for="file_video_title">توضیح فایل صوتی :</label>
+                            <input type="text" class="form-control" name="file_video_title">
+                        </div>
                     </div>
 
-                    <div class="col-md-8 mt-2">
-                        <label for="file_video_doc">توضیح فایل متنی :</label>
-                        <input type="text" class="form-control" name="file_doc_2_title">
+                    <div class="row">
+                        <div class="col-md-4 mt-2">
+                            <label for="file_video_2">آپلود ویدیو  :</label>
+                            <input type="file" name="file_video_2" accept="video/*" class="form-control">
+                        </div>
 
+                        <div class="col-md-8 mt-2">
+                            <label for="file_video_2_title">توضیح فایل ویدیویی  :</label>
+                            <input type="text" class="form-control" name="file_video_2_title">
+                        </div>
                     </div>
+
+
+                    <div class="row">
+                        <div class="col-md-4 mt-2">
+                            <label for="file_doc">آپلود فایل متنی :</label>
+                            <input type="file" name="file_doc_2" class="form-control">
+                        </div>
+
+                        <div class="col-md-8 mt-2">
+                            <label for="file_video_doc">توضیح فایل متنی :</label>
+                            <input type="text" class="form-control" name="file_doc_2_title">
+
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-4 mt-2">
+                            <label for="zip_file"> فایل فشرده : </label>
+                            <input type="file" name="zip_file" accept=".zip,.rar" class="form-control">
+                            <small>تمام فایل اضافی زیپ شده و این بخش آپلود شود</small>
+                        </div>
+
+                        <div class="col-md-8 mt-2">
+                            <label for="show"> فایل های اضافه در این بخش توضیح داده شود  :</label>
+                            <input type="text" class="form-control" name="show">
+                    
+                        </div>
+                    </div>
+
+
                 </div>
 
-                <div class="row">
-                    <div class="col-md-4 mt-4">
-                        <label for="zip_file"> فایل فشرده : </label>
-                        <input type="file" name="zip_file" class="form-control">
-                        <small>تمام فایل اضافی زیپ شده و این بخش آپلود شود</small>
-                    </div>
 
-                    <div class="col-md-8 mt-4">
-                        <label for="show"> فایل های اضافه در این بخش توضیح داده شود  :</label>
-                        <input type="text" class="form-control" name="show">
-                   
-                    </div>
-                </div>
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40;margin-bottom:2px;">برای ایجاد تکلیف ابتدا فایل تکلیف را آپلود و زمان تحویل تکلیف را انتخاب کنید</p>
+                    <p style="color: #A63D40;font-size:12px"> در صورت آپلود تکلیف حتما تاریخ تحویل را تعیین کنید </p>
 
-                <div class="row mt-5">
-                    <div class="col-md-3 mt-2">
-                        <label for="file_doc_2">آپلود تکلیف  :</label>
-                        <input type="file" name="file_doc" class="form-control">
+                    <div class="row mt-5">
+                        <div class="col-md-3 mt-2">
+                            <label for="file_doc_2">آپلود تکلیف  :</label>
+                            <input type="file" name="file_doc" class="form-control">
 
-                    </div>
+                        </div>
 
-                    <div class="col-md-6 mt-2">
-                        <label for="file_doc_2_title">توضیح فایل  تکلیف :</label>
-                        <input type="text" class="form-control" name="file_doc_title">
-                    </div>
+                        <div class="col-md-7 mt-2">
+                            <label for="file_doc_2_title">توضیح فایل  تکلیف :</label>
+                            <input type="text" class="form-control" name="file_doc_title">
+                        </div>
 
-                    <div class="col-md-3 mt-2">
-                        <label for="file_doc_2_title"> تاریخ پایان تحویل :</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text cursor-pointer" id="date_select" data-mdpersiandatetimepicker="" data-mdpersiandatetimepicker-group="rangeSelector1" data-fromdate="" data-uniqueid="1601319385285" data-original-title="" title="">تقویم</span>
+                        <div class="col-md-2 mt-2">
+                            <label for="file_doc_2_title"> تاریخ پایان تحویل :</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text cursor-pointer" id="date_select" data-mdpersiandatetimepicker="" data-mdpersiandatetimepicker-group="rangeSelector1" data-fromdate="" data-uniqueid="1601319385285" data-original-title="" title="">تقویم</span>
+                                    </div>
+                                    <input type="text" name="timer" id="input_date_select" class="form-control" placeholder="  تاریخ" aria-label="date4" aria-describedby="date4" readonly>
                                 </div>
-                                <input type="text" name="timer" id="input_date_select" class="form-control" placeholder="مشاهده برای تاریخ" aria-label="date4" aria-describedby="date4" readonly>
-                            </div>
-                            <small id="showDate_class" style="color:#A63D40">دوشنبه ۰۴ شهریور ۱۳۹۸</small>
+                                <small id="showDate_class" style="color:#A63D40;font-size:16px">دوشنبه ۰۴ شهریور ۱۳۹۸</small>
 
+                        </div>
                     </div>
+
+
+                    <input type="text" name="teacher_upload_id" value="{{Auth::user()->id}}" hidden>
+                    <input type="text" name="course_id" value="{{$course->id}}" hidden>
+
                 </div>
 
-                <p style="color: #A63D40;font-size:12px;"> در صورت آپلود تکلیف حتما تاریخ تحویل را تعیین کنید </p>
-
-                <input type="text" name="teacher_upload_id" value="{{Auth::user()->id}}" hidden>
-                <input type="text" name="course_id" value="{{$course->id}}" hidden>
-
-                <div class="row">
-                    <div class="col-md-12 mt-5" style="text-align: center;">
-                        <button class="btn btn-success" style="padding:5px 20% 5px 20%;">آپلود جلسه</button>
+                    <div class="row">
+                        <div class="col-md-12 mt-5" style="text-align: center;">
+                            <button class="btn btn-success" style="padding:5px 20% 5px 20%;">آپلود جلسه</button>
+                        </div>
                     </div>
-                </div>
+
             </div>
             
         </div>
--- resources/views/teacher/edit_assigment.blade.php
+++ resources/views/teacher/edit_assigment.blade.php
@@ -32,113 +32,177 @@
 <input type="text" name="course_id" value="{{$course->id}}" hidden>
 
                 <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4 mt-3">
-                        <label for="title">عنوان جلسه :</label>
-                        <input type="text" class="form-control" value="{{$assignment->title}}" name="title">
-                    </div>
-                    <div class="col-md-8 mt-3">
-                        <label for="desc"> شرح مختصر جلسه :</label>
-                        <input type="text" class="form-control" value="{{$assignment->desc}}" name="desc">
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-md-12 mt-3">
-                        <label for="desc_2">توضیحات کامل :</label>
-                        <textarea name="desc_2" class="form-control" rows="4">{{$assignment->desc_2}}</textarea>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4 mt-4">
-                        <label for="file_video">آپلود فایل صوتی :</label>
-                        <input type="file" name="file_video" class="form-control">
-                        <small><a href="{{$assignment->file_video}}">دانلود</a></small>
-
-                    </div>
-
-                    <div class="col-md-8 mt-4">
-                        <label for="file_video_title">توضیح فایل صوتی :</label>
-                        <input type="text" class="form-control" value="{{$assignment->file_video_title}}" name="file_video_title">
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4 mt-2">
-                        <label for="file_video_2">آپلود ویدیو  :</label>
-                        <input type="file" name="file_video_2" class="form-control">
-                        <small><a href="{{$assignment->file_video_2}}">دانلود</a></small>
-
-                    </div>
-
-                    <div class="col-md-8 mt-2">
-                        <label for="file_video_2_title">توضیح فایل ویدیویی  :</label>
-                        <input type="text" class="form-control" value="{{$assignment->file_video_2_title}}" name="file_video_2_title">
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40">می توانید در این بخش عنوان یا شرح جلسه را اصلاح کنید، لطفا  این  دو فیلد را خالی نگذارید</p>
+                    <div class="row">
+                        <div class="col-md-4 mt-3">
+                            <label for="title">عنوان جلسه :</label>
+                            <input type="text" class="form-control" value="{{$assignment->title}}" name="title">
+                        </div>
+                        <div class="col-md-8 mt-3">
+                            <label for="desc"> شرح مختصر جلسه :</label>
+                            <input type="text" class="form-control" value="{{$assignment->desc}}" name="desc">
+                        </div>
                     </div>
                 </div>
 
 
-                <div class="row">
-                    <div class="col-md-4 mt-2">
-                        <label for="file_doc">آپلود فایل متنی :</label>
-                        <input type="file" name="file_doc_2" class="form-control">
-                        <small><a href="{{$assignment->file_doc_2}}">دانلود</a></small>
-
-                    </div>
-
-                    <div class="col-md-8 mt-2">
-                        <label for="file_video_doc">توضیح فایل متنی :</label>
-                        <input type="text" class="form-control" value="{{$assignment->file_video_doc}}" name="file_doc_2_title">
-
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4 mt-4">
-                        <label for="zip_file"> فایل فشرده : </label>
-                        <input type="file" name="zip_file" class="form-control">
-                        <small><a href="{{$assignment->zip_file}}">دانلود</a></small>
-
-                    </div>
-
-                    <div class="col-md-8 mt-4">
-                        <label for="show"> فایل های اضافه در این بخش توضیح داده شود  :</label>
-                        <input type="text" class="form-control" value="{{$assignment->show}}" name="show">
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40">در این بخش میتوانید توضیحات کامل را ویرایش یا پاک کنید</p>
                    
+                    <div class="row">
+                        <div class="col-md-12 mt-3">
+                            <label for="desc_2">توضیحات کامل :</label>
+                            <textarea name="desc_2" class="form-control" rows="4">{{$assignment->desc_2}}</textarea>
+                        </div>
                     </div>
+                </div>    
+
+
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    <p style="font-size:11px;color:#A63D40;margin-bottom:3px;">در صورتی که فایلی آپلود شده باشد از بخش اول هر ردیف قابل "دانلود" است</p>
+                    <p style="font-size:11px;color:#A63D40;margin-bottom:3px;">در صورتی که فایلی آپلود نشده باشد بخش دانلود "غیرفعال" است</p>
+                    <p style="font-size:11px;color:#A63D40">در صورتی که میخاهید فایلی حذف شود در توضیحات همان فایل کلمه "حذف" را بنویسید</p>
+
+                    <div class="row">
+
+                        <div class="col-md-1 col-3 mt-5">
+                            @if(strlen($assignment->file_video) > 5)
+                                <a href="{{$assignment->file_video}}">
+                                <button class="btn btn-success" style="margin-top:6px;">دانلود</button>
+                                </a>
+                            @else
+                                <button class="btn btn-secondary" style="margin-top:6px;" disabled>غیرفعال</button>
+                            @endif
+
+                        </div>
+
+                        <div class="col-md-3 col-9 mt-4">
+                            <label for="file_video">آپلود فایل صوتی :</label>
+                            <input type="file" name="file_video" accept="audio/*" class="form-control">
+
+                        </div>
+
+                        <div class="col-md-8 mt-4">
+                            <label for="file_video_title">توضیح فایل صوتی :</label>
+                            <input type="text" class="form-control" value="{{$assignment->file_video_title}}" name="file_video_title">
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-md-1 col-3 mt-4">
+                                @if(strlen($assignment->file_video_2) > 5)
+                                    <a href="{{$assignment->file_video_2}}">
+                                    <button class="btn btn-success" style="margin-top:12px;">دانلود</button>
+                                    </a>
+                                @else
+                                    <button class="btn btn-secondary" style="margin-top:12px;" disabled>غیرفعال</button>
+                                @endif
+
+                        </div>
+                        <div class="col-md-3 col-9 mt-2">
+                            <label for="file_video_2">آپلود ویدیو  :</label>
+                            <input type="file" name="file_video_2" accept="video/*" class="form-control">
+                        </div>
+
+                        <div class="col-md-8 mt-2">
+                            <label for="file_video_2_title">توضیح فایل ویدیویی  :</label>
+                            <input type="text" class="form-control" value="{{$assignment->file_video_2_title}}" name="file_video_2_title">
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+
+                        <div class="col-md-1 col-3 mt-4">
+                                @if(strlen($assignment->file_doc_2) > 5)
+                                    <a href="{{$assignment->file_doc_2}}">
+                                    <button class="btn btn-success" style="margin-top:12px;">دانلود</button>
+                                    </a>
+                                @else
+                                    <button class="btn btn-secondary" style="margin-top:12px;" disabled>غیرفعال</button>
+                                @endif
+
+                        </div>
+
+                        <div class="col-md-3 col-9 mt-2">
+                            <label for="file_doc">آپلود فایل متنی :</label>
+                            <input type="file" name="file_doc_2" class="form-control">
+
+                        </div>
+
+                        <div class="col-md-8 mt-2">
+                            <label for="file_video_doc">توضیح فایل متنی :</label>
+                            <input type="text" class="form-control" value="{{$assignment->file_video_doc}}" name="file_doc_2_title">
+
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-md-1 col-3 mt-4">
+                                @if(strlen($assignment->show) > 5)
+                                    <a href="{{$assignment->show}}">
+                                    <button class="btn btn-success" style="margin-top:12px;">دانلود</button>
+                                    </a>
+                                @else
+                                    <button class="btn btn-secondary" style="margin-top:12px;" disabled>غیرفعال</button>
+                                @endif
+
+                        </div>
+                        <div class="col-md-3 col-9 mt-2">
+                            <label for="zip_file"> فایل فشرده : </label>
+                            <input type="file" name="zip_file" accept=".zip,.rar" class="form-control">
+
+                        </div>
+
+                        <div class="col-md-8 mt-2">
+                            <label for="show"> فایل های اضافه در این بخش توضیح داده شود  :</label>
+                            <input type="text" class="form-control" value="{{$assignment->show}}" name="show">
+                    
+                        </div>
+                    </div>
+
                 </div>
 
-                <div class="row mt-5">
-                    <div class="col-md-3 mt-2">
-                        <label for="file_doc">آپلود تکلیف  :</label>
-                        <input type="file" name="file_doc" class="form-control">
-                        <small><a href="{{$assignment->file_doc}}">دانلود</a></small>
+                <div style="padding:20px;border:.4px solid #0892A5;border-radius:15px;margin-top:20px">
+                    @if(App\Models\Exercisenotice::where('assginment_id',$assignment->id)->exists())
+                    <p style="font-size:14px;color:#0C8346;margin-bottom:5px;">برای دانلود تکلیف اینجا کلیک کنید</p>
 
-                    </div>
+                    <p style="font-size:11px;color:#A63D40;margin-bottom:3px;">در صورت ویرایش کردن تکلیف حتما تاریخ تحویل تکلیف را وارد چک کنید</p>
+                    @else
+                    <p style="font-size:11px;color:#A63D40;margin-bottom:3px;">در صورت اضافه کردن تکلیف حتما تاریخ تحویل تکلیف را وارد کنید</p>
+                    @endif
+                    <div class="row mt-5">
+                        <div class="col-md-3 mt-2">
+                            <label for="file_doc">آپلود تکلیف  :</label>
+                            <input type="file" name="file_doc" class="form-control">
+                            <small><a href="{{$assignment->file_doc}}">دانلود</a></small>
 
-                    <div class="col-md-6 mt-2">
-                        <label for="file_doc_2_title">توضیح فایل  تکلیف :</label>
-                        <input type="text" class="form-control" value="{{$assignment->file_doc_2_title}}" name="file_doc_title">
-                    </div>
+                        </div>
 
-                    <div class="col-md-3 mt-2">
-                        <label for="file_doc_2_title"> تاریخ پایان تحویل :</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text cursor-pointer" id="date_select" data-mdpersiandatetimepicker="" data-mdpersiandatetimepicker-group="rangeSelector1" data-fromdate="" data-uniqueid="1601319385285" data-original-title="" title="">تقویم</span>
+                        <div class="col-md-6 mt-2">
+                            <label for="file_doc_2_title">توضیح فایل  تکلیف :</label>
+                            <input type="text" class="form-control" value="{{$assignment->file_doc_2_title}}" name="file_doc_title">
+                        </div>
+
+                        <div class="col-md-3 mt-2">
+                            <label for="file_doc_2_title"> تاریخ پایان تحویل :</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text cursor-pointer" id="date_select" data-mdpersiandatetimepicker="" data-mdpersiandatetimepicker-group="rangeSelector1" data-fromdate="" data-uniqueid="1601319385285" data-original-title="" title="">تقویم</span>
+                                    </div>
+                                    <input type="text" value="{{$assignment->file_doc_2_title}}"  name="timer" id="input_date_select" class="form-control" placeholder="مشاهده برای تاریخ" aria-label="date4" aria-describedby="date4" readonly>
                                 </div>
-                                <input type="text"  name="timer" id="input_date_select" class="form-control" placeholder="مشاهده برای تاریخ" aria-label="date4" aria-describedby="date4" readonly>
-                            </div>
-                            <small id="showDate_class" style="color:#A63D40">دوشنبه ۰۴ شهریور ۱۳۹۸</small>
+                                <small id="showDate_class" style="color:#A63D40">دوشنبه ۰۴ شهریور ۱۳۹۸</small>
 
+                        </div>
                     </div>
+                
                 </div>
 
-                <p style="color: #A63D40;font-size:12px;"> در صورت آپلود تکلیف حتما تاریخ تحویل را تعیین کنید </p>
+<input type="text" name="teacher_upload_id" value="{{Auth::user()->id}}" hidden>
+<input type="text" name="course_id" value="{{$course->id}}" hidden>
 
-                <input type="text" name="teacher_upload_id" value="{{Auth::user()->id}}" hidden>
-                <input type="text" name="course_id" value="{{$course->id}}" hidden>
 
                 <div class="row">
                     <div class="col-md-12 mt-5" style="text-align: center;">
